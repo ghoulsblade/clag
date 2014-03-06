89bc35d8be3107028b2581325825d829773c40d3
ghoulsblade <ghoulsblade@schattenkind.net>
2014-03-06 20:47:50 +0100

    release clag-v0.0.1.zip, increase version.

--- src/main/java/clag/CLagInfo.java
+++ src/main/java/clag/CLagInfo.java
@@ -3,7 +3,7 @@ package clag;
 public class CLagInfo {
 	public static final String ID		= "CLagID";
 	public static final String NAME		= "CLag";
-	public static final String VERS		= "0.0.1";
+	public static final String VERS		= "0.0.2";
 	public static final String CLIENTPROXY		= "clag.ClientProxy";
 	public static final String COMMONPROXY		= "clag.CommonProxy";
 }
